5a51cef96b63d466d145280c9ed989056c4d11b6
Pibsu <paulbennedict.pitas.eng@ust.edu.ph>
2019-05-21 19:49:19 +0800

    second commit

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 int main()
 {
-	char arr[7];
-	int x, y, temp;
-
-	for (x = 0; x < 7; x++)
+	char arr[100];
+	int x, y, temp, n;
+	
+	cout << "Please enter the number of characters that you wish to enter: "; cin >> n;
+	for (x = 0; x < n; x++)
 	{
 		cout << "Enter a character: "; cin >> arr[x];
 	}
-	for (x = 0; x < 7; x++)
+	for (x = 0; x < n; x++)
 	{
-		for (y = 0; y < 7 - x - 1; y++)
+		for (y = 0; y < n - x - 1; y++)
 		{
 			if (arr[y] > arr [y+1])
 			{
@@ -26,7 +27,7 @@ int main()
 	}
 	cout << "+++++++++++++++++++++++++\n \n";
 	cout << "The array's elements are:\n";
-	for (x = 0; x <= 6; x++)
+	for (x = 0; x <= n - 1; x++)
 	{
 		cout << arr[x];
 	}
